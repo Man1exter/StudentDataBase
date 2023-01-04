4eaf62131a329ce79a77322be8b8b7d9f92f5121
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-04 17:06:50 +0100

    section - menu

--- dbstsu.py
+++ dbstsu.py
@@ -1,3 +1,30 @@
 import random
 
-# vdvdvdvdv
+def menu():
+    
+    print(" ")
+    print("[1] - ADD NEW STUDENT")
+    print("[2] - ADD DELETE STUDENT")
+    print("[3] - SHOW ALL STUDENTS")
+    print(" ")
+    
+
+def main():
+    
+    log = input("LOGIN: ")
+    pwd = input("PASSWORD: ")
+    
+    if log == "admin" and pwd == "admin" :
+        
+       print(" ")
+       print("WELCOME " + log + " " + pwd)
+      
+       menu()
+      
+    else:
+        print(" ")
+        print("Suspicious..")
+        return 0
+    
+main()
+    
