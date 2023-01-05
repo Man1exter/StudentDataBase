4389bb0d33035550e2a7769b39accc0bf9f80b7a
Mariusz Perzynski <m.perzyk@interia.pl>
2023-01-05 16:04:00 +0100

    set file with read/write/abw

--- dbstsu.py
+++ dbstsu.py
@@ -4,7 +4,7 @@ def menu():
     
     print(" ")
     print("[1] - ADD NEW STUDENT")
-    print("[2] - ADD DELETE STUDENT")
+    print("[2] - DELETE STUDENT")
     print("[3] - SHOW ALL STUDENTS")
     print("[4] - EXIT QUIT CLOSE")
     print(" ")
@@ -12,34 +12,61 @@ def menu():
     
 def stu():
     
+    file = "studen.txt"
+    fi = open(file, "a", encoding="utf-8")
+    
     print(" ")
     print(" ")
     
     list_base = []
     
     
-    names = input("Name: ")
-    surnames = input("Surname: ")
-    ages = input("Age: ")
-    classes = input("Class: ")
-    cities = input("City: ")
+    names = input(" Name: ")
+    surnames = input(" Surname: ")
+    ages = input(" Age: ")
+    classes = input(" Class: ")
+    cities = input(" City: ")
     
-    human = names + " " + surnames + " " + ages + " " + classes + " " + cities
+    human = " " + names + " " + surnames + " " + ages + " " + classes + " " + cities
     
+    fi.write(human + "\n")
     
     list_base.append(human)
     
     print(" ")
     
-    print("NEW STUDENT => ")
+    print("NEW STUDENT ADD TO YOUR DATABASE=> ")
     
     print(" ")
     
     for x in list_base:
         print(x)
+        
+    fi.close()
+    
+def show_student():
+    
+    file = "studen.txt"
+    fi = open(file, "r", encoding="utf-8")
+    
+    # SOLO LINE FROM FILE
+    # print(fi.readline())
+    # fi.readline()
+    
+    for lines in fi:
+        print(lines)
+    
+    fi.close()
+    
     
 
 def main():
+    
+    #  fi = open(file,"r")
+    
+    # r - read, w - write, a - add new value ( dont delete ctr )
+    
+    #  file = "studen.txt"
     
     log = input("LOGIN: ")
     pwd = input("PASSWORD: ")
@@ -58,12 +85,12 @@ def main():
        elif goal == "2":
            pass
        elif goal == "3":
-           pass
+           show_student()
        elif goal == "4":
            print(" ")
            print("QUIT FROM THE BASE " + log + " " + pwd)
        else:
-           print(" UNKNOWN ")
+           print(" UNKNOWN COMMAND ")
       
     else:
         print(" ")
